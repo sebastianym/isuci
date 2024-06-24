32434e7a1441c3b5e2d612814071c4adc668907e
Sebastián Yepes <syepesm05@gmail.com>
2024-06-24 16:39:50 -0500

    Login

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import Link from "next/link";
+import { ChangeEvent, useState } from "react";
+
+function Login() {
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [infoFormulario, setInfoFormulario] = useState({
+    usuarioInput: "",
+    contraseñaInput: "",
+  });
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsuario(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setContraseña(event.target.value);
+  };
+
+  const checkInfoForm = () => {
+    const updatedInfoForm = { ...infoFormulario };
+
+    usuario === ""
+      ? (updatedInfoForm.usuarioInput = "Usuario obligatorio")
+      : (updatedInfoForm.usuarioInput = "");
+    contraseña === ""
+      ? (updatedInfoForm.contraseñaInput = "Contraseña obligatoria")
+      : (updatedInfoForm.contraseñaInput = "");
+
+    setInfoFormulario(updatedInfoForm);
+
+    if (usuario != "" && contraseña != "") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleLogin();
+    }
+  };
+
+  const handleLogin = async () => {
+    const inputValidation = checkInfoForm();
+  };
+
+  return (
+    <div className="md:p-20 h-screen max-md:py-10  flex justify-center items-center">
+      <div className="w-full max-xl:w-full container">
+        <div className="w-full flex flex-wrap bg-bg-dark-secondary rounded-2xl">
+          <div className="hidden lg:block lg:w-1/2 lg:h-auto w-full h-52 relative lg:rounded-l-2xl max-lg:rounded-xl lg:order-2 order-2 max-lg:mt-5">
+            <div
+              className="absolute inset-0 lg:rounded-l-2xl rounded-b-2xl brightness-[0.8]"
+              style={{
+                backgroundImage: "url('/images/imagen-login.png')",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            />
+          </div>
+          <div className="lg:w-1/2 mt-5 xl:p-20 lg:!p-12 p-12 !pb-6 max-sm:p-14 rounded-r-2xl max-lg:rounded-xl lg:order-1 order-1">
+            {infoFormulario.usuarioInput && (
+              <p className="text-white font-bold bg-red-500 text-center p-2 text-sm select-none mb-2">
+                {infoFormulario.usuarioInput}
+              </p>
+            )}
+            {infoFormulario.contraseñaInput && (
+              <p className="text-white font-bold bg-red-500 text-center p-2 text-sm select-none">
+                {infoFormulario.contraseñaInput}
+              </p>
+            )}
+            <div className="my-5">
+              <h1 className="text-3xl font-semibold xl:mb-8 mb-10 w-full py-5 text-white">
+                ISUCI 🚵
+              </h1>
+            </div>
+            <h2 className="text-xl font-semibold mb-3 text-white/80">
+              Iniciar sesión
+            </h2>
+            <p className="mb-8 text-bg-black max-xl:text-justify text-white/50">
+              A continuación ingresa tus datos para acceder al sistema.
+            </p>
+            <form>
+              <label>
+                <p className="text-white/80 font-medium">
+                  Usuario
+                  <br />
+                </p>
+              </label>
+              <input
+                className="w-full h-12  pl-5 pr-3 rounded-md mb-5 mt-2 bg-white/10 border-none focus:outline-none text-white/50 placeholder:text-white/20"
+                type="email"
+                value={usuario}
+                onChange={handleUsernameChange}
+                placeholder="Ingresa tu usuario"
+                onKeyPress={handleKeyPress}
+                autoComplete="username"
+              />
+              <br />
+              <label>
+                <p className="text-white/80 font-medium">
+                  Contraseña
+                  <br />
+                </p>
+              </label>
+              <input
+                className="w-full h-12  pl-5 pr-3 rounded-md mb-7 mt-2 bg-white/10 border-none focus:outline-none text-white/50 placeholder:text-white/20"
+                type="password"
+                value={contraseña}
+                onChange={handlePasswordChange}
+                onKeyPress={handleKeyPress}
+                placeholder="Ingresa tu contraseña"
+                autoComplete="current-password"
+              />
+              <br />
+              <button
+                className="w-full bg-bg-green-secondary hover:bg-[#3C5B6F] transition-all h-14 rounded-md text-white font-medium mb-3"
+                type="button"
+                onClick={handleLogin}
+              >
+                Iniciar sesión
+              </button>
+              <Link
+                href={"/"}
+                className="w-fit mb-1 mt-5 font-medium block text-gray-white"
+              >
+                ¿Olvidaste tu contraseña?
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // Primary background color
+
+        "bg-dark-primary": "#1f2125",
+        "bg-white-primary": "#eaeaec",
+
+        // Secondary background color
+        "bg-dark-secondary": "#161819",
+        "bg-white-secondary": "#f5f7f9",
+        "bg-green-secondary": "#577B8D",
+
+        // Text color
+
+        white: "#ffffff",
+        black: "#000000",
+        "gray-dark": "#bababa",
+        "gray-white": "#868893",
+
+        "bg-dark-hover": "#313234",
+        "bg-white-hover": "#d9d9d9",
+        "bg-input-error": "#8d3c3c",
+      },
     },
   },
   plugins: [],
